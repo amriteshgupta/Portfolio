d86eb6e5f4fcefd9027f5ae1a421d8e752fedc99
Amritesh <48851357+amriteshgupta@users.noreply.github.com>
2024-03-27 00:05:26 +0530

    Update index.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -33,7 +33,7 @@ const About = () => {
             />
           </h1>
           <p>
-            I'm a very ambitious front-end developer looking for a role in an
+            I'm a very ambitious developer looking for a role in an
             established IT company with the opportunity to work with the latest
             technologies on challenging and diverse projects.
           </p>
@@ -42,8 +42,7 @@ const About = () => {
             improving my chops one design problem at a time.
           </p>
           <p>
-            If I need to define myself in one sentence that would be a family
-            person, father of a beautiful daughter, a sports fanatic,
+            If I need to define myself in one sentence that would be a sports fanatic,
             photography enthusiast, and tech-obsessed!!!
           </p>
         </div>
